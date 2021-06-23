2feec32e974f5b0f987dbbe6f3b3bf6dbcb113f6
pbrocketmoj <85356666+pbrocketmoj@users.noreply.github.com>
2021-06-23 16:56:46 +0100

    removing text

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
 // Add your routes here - above the module.exports line
+router.post('/add-visitors-answer', function (req, res) {
+
+ var addVisitors = req.session.data['add-visitors']
+
+ if (addVisitors == "yes"){
+   // Send user to next page
+   res.redirect('/VJ02_book_a_visit_2/addvisitors2')
+ } else {
+   // Send user to ineligible page
+   res.redirect('/VJ02_book_a_visit_2/adjustments')
+ }
+
+})
 
 module.exports = router
+
