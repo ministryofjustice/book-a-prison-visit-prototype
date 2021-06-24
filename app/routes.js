81413be9e90e23850c701ea95b2caf11575ff74c
pbrocketmoj <85356666+pbrocketmoj@users.noreply.github.com>
2021-06-24 15:59:29 +0100

    hoping this fixes it

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const radioButtonRedirect = require('radio-button-redirect')
-router.use(radioButtonRedirect)
-
-// Add your routes here - above the module.exports line
-router.post('/add-visitors-answer', function (req, res) {
-
- var addVisitors = req.session.data['add-visitors']
-
- if (addVisitors == "yes"){
-   // Send user to next page
-   res.redirect('/VJ02_book_a_visit_2/addvisitors2')
- } else {
-   // Send user to adjustments page
-   res.redirect('/VJ02_book_a_visit_2/adjustments')
- }
-
-})
 
 module.exports = router
 
