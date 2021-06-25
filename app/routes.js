9d4e20009631db990ee3704482c9222e72971564
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-06-25 10:46:24 +0100

    Add route switching and fix buttons

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const parseUrl = require("parse-url")
 
+router.post('/add-visitors-answer', function (req, res) {
+    var addVisitors = req.session.data['add-visitors']
+    const { pathname } = parseUrl(req.headers.referer)
+    const prefix = pathname.split('/')
+    
+    if (addVisitors == "yes"){
+      res.redirect(`/${prefix[1]}/addvisitors2`)
+    } else {
+      res.redirect(`/${prefix[1]}/adjustments`)
+    }
+  
+  })
 
 module.exports = router
 
